39541d21641f97c00da10cc28fbf78cce47f5c8e
Daniel Chicot <dan.chicot@gmail.com>
2020-04-08 14:40:37 +0100

    Initial import.

---
+++ decrypt_output.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import argparse
+import base64
+import binascii
+import gzip
+import sys
+
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+
+
+# Decrypt sample input with key: '2KuX63Oh+nlRFRLIrxib3g==', iv: 'VaZkOTf+fn2YI1uy/snFGg=='
+
+def main():
+    args = command_line_args()
+    with open(args.encrypted_file, '+rb') as encrypted_file:
+        encrypted = encrypted_file.read()
+        decrypted = decrypt(args.data_key, args.iv, encrypted)
+        final = gzip.decompress(decrypted) if args.decompress else decrypted
+        sys.stdout.buffer.write(final)
+
+
+def decrypt(key, iv, ciphertext):
+    iv_int = int(binascii.hexlify(base64.b64decode(iv)), 16)
+    ctr = Counter.new(AES.block_size * 8, initial_value=iv_int)
+    aes = AES.new(base64.b64decode(key), AES.MODE_CTR, counter=ctr)
+    return aes.decrypt(ciphertext)
+
+
+def command_line_args():
+    parser = argparse.ArgumentParser(description='Decrypt files produced by htme.')
+    parser.add_argument('-i', '--iv', help='The initialisation vector used to encrypt.', required=True)
+    parser.add_argument('-k', '--data-key', help='The plaintext decryption key.', required=True)
+    parser.add_argument('-z', '--decompress', help='Decompress the decrypted text.', action='store_true')
+    parser.add_argument('encrypted_file', help='The encrypted file.')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
